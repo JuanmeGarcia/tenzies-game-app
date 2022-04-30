c7139b830754026bc3a17556bfa7c421a14e7842
Juanme <jmgcasd@gmail.com>
2022-04-29 23:06:03 -0300

    add some extras

--- src/App.js
+++ src/App.js
@@ -8,93 +8,100 @@ import Header from './components/Header'
 import Confetti from './components/Confetti'
 
 function App() {
-    const [counter, setCounter] = React.useState(0)
+	const [counter, setCounter] = React.useState(0)
 	const [dices, setDices] = React.useState(newDice())
-    const [tenzies, setTenzies] = React.useState(false)
+	const [tenzies, setTenzies] = React.useState(false)
+	const [timeTracker, setTimeTracker] = React.useState(0)
 
-    React.useEffect(()=>{
-        const dicesAreEqual = dices.every(dice => 
-            ((dice.value === dices[0].value) && dice.isHeld)
-            )
+	React.useEffect(() => {
+		const dicesAreEqual = dices.every(
+			dice => dice.value === dices[0].value && dice.isHeld
+		)
 
-        if(dicesAreEqual){
-            setTenzies(true)
-            console.log('you won bitch')
-        }
-    }, [dices])
+		if (dicesAreEqual) {
+			setTenzies(true)
+			console.log('you won bitch')
+		}
+	}, [dices])
 
-    function addToCounter(){
-        setCounter(prevCounter => prevCounter + 1)
-    }
+	function addToCounter() {
+		setCounter(prevCounter => prevCounter + 1)
+	}
 
-    function restarToZero(){
-        setCounter(0)
-    }
+	function restarToZero() {
+		setCounter(0)
+	}
 
 	function newDice() {
 		const diceNumbers = []
 		for (let i = 0; i < 10; i++) {
-            diceNumbers.push({
-                value: Math.ceil(Math.random() * 6), 
-                isHeld: false,
-                id: nanoid()
-            })
+			diceNumbers.push({
+				value: Math.ceil(Math.random() * 6),
+				isHeld: false,
+				id: nanoid(),
+			})
 		}
 		return diceNumbers
 	}
 
 	function updateDiceNumber() {
-        addToCounter()
-		setDices(prevDices=>(
-            prevDices.map(dice=>(
-                !dice.isHeld ? 
-                {...dice,
-                    value: Math.ceil(Math.random() * 6)
-                } 
-                : dice
-            ))
-        ))
-    }
+		addToCounter()
+		setDices(prevDices =>
+			prevDices.map(dice =>
+				!dice.isHeld
+					? { ...dice, value: Math.ceil(Math.random() * 6) }
+					: dice
+			)
+		)
+	}
 
-    function restarGame(){
-        setDices(newDice())
-        setTenzies(false)
-        restarToZero()
-    }
+	function restarGame() {
+		setDices(newDice())
+		setTenzies(false)
+		restarToZero()
+        setTimeTracker(0)
+		clearInterval(timeTracker)
+	}
 
-    function holdDice(id){
-        setDices((prevDices)=>(
-            prevDices.map((dice)=>{
-                return dice.id === id ? 
-                    {...dice,
-                    isHeld: !dice.isHeld
-                    } 
-                    : dice
-            })
-        ))
-    }
+	function holdDice(id) {
+		setDices(prevDices =>
+			prevDices.map(dice => {
+				return dice.id === id ? { ...dice, isHeld: !dice.isHeld } : dice
+			})
+		)
+	}
+
+	React.useEffect(()=>{
+        const timer = setInterval(()=>{
+            setTimeTracker(prevTimeTracker => prevTimeTracker + 1)
+            console.log('timer', timeTracker)
+        }, 1000)
+
+    }, [])
 
 	return (
 		<main className="main">
-            <Header counter={counter}/>
+			<Header counter={counter} timeTracker={timeTracker}/>
 			<Container>
 				{dices.map((dice, index) => {
-					return <Dice 
-                        key={index} 
-                        value={dice.value} 
-                        isHeld={dice.isHeld} 
-                        holdDice={()=>holdDice(dice.id)}
-                    />
+					return (
+						<Dice
+							key={index}
+							value={dice.value}
+							isHeld={dice.isHeld}
+							holdDice={() => holdDice(dice.id)}
+						/>
+					)
 				})}
 			</Container>
-			<Button 
-                updateDiceNumber={updateDiceNumber}
-                restarGame={restarGame}
-                dices={dices}
-                tenzies={tenzies}
-            />
+			<Button
+				updateDiceNumber={updateDiceNumber}
+				restarGame={restarGame}
+				dices={dices}
+				tenzies={tenzies}
+			/>
 
-            {tenzies && <Confetti />}
+			{tenzies && <Confetti />}
 		</main>
 	)
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function Header(props) {
 	return (
 		<header className='header'>
-            <p className='counter'>Counter: {props.counter}</p>
+            <p className='counter'>Counter: {props.counter} </p>
+            <p className='timer'>Timer: {props.timeTracker}</p>
 			<h1 className='header__title'>Tenzies</h1>
 			<h3 className='header__subtitle'>
 				Roll until all dice are the same. Click each die to freeze it at
